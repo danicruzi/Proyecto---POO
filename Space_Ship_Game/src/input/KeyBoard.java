2d71716f0fa5bc6f5d1f99f029269e8738b77e6d
danicruzi <dani.cruz0207@gmail.com>
2025-05-19 22:49:11 -0600

    Add files via upload

---
+++ Space_Ship_Game/src/input/KeyBoard.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package input;
+
+/**
+ *
+ * @author Daniel
+ */
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoard implements KeyListener{
+	
+	private boolean[] keys = new boolean[256];
+	
+	public static boolean UP, LEFT, RIGHT, SHOOT;
+	
+	public KeyBoard()
+	{
+		UP = false;
+		LEFT = false;
+		RIGHT = false;
+		SHOOT = false;
+	}
+	
+	public void update()
+	{
+		UP = keys[KeyEvent.VK_UP];
+		LEFT = keys[KeyEvent.VK_LEFT];
+		RIGHT = keys[KeyEvent.VK_RIGHT];
+		SHOOT = keys[KeyEvent.VK_P];
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	
+}
