114aac686a616075917f10d42bd893ca100a6e29
danicruzi <dani.cruz0207@gmail.com>
2025-05-19 22:53:09 -0600

    Add files via upload

---
+++ Space_Ship_Game/src/ui/Action.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ui;
+
+/**
+ *
+ * @author Farid Simon
+ */
+public interface Action {
+	
+	public abstract void doAction();
+	
+}
