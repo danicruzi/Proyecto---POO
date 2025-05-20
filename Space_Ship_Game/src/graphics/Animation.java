b226c7271ab60b64fd311db284d1d405dca98a6c
danicruzi <dani.cruz0207@gmail.com>
2025-05-19 22:47:57 -0600

    Add files via upload

---
+++ Space_Ship_Game/src/graphics/Animation.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphics;
+
+import java.awt.image.BufferedImage;
+
+import math.Vector2D;
+
+/**
+ *
+ * @author Farid Simon
+ */
+public class Animation {
+	
+	private BufferedImage[] frames;
+	private int velocity;
+	private int index;
+	private boolean running;
+	private Vector2D position;
+	private long time;
+	
+	public Animation(BufferedImage[] frames, int velocity, Vector2D position){
+		this.frames = frames;
+		this.velocity = velocity;
+		this.position = position;
+		index = 0;
+		running = true;
+	}
+	
+	public void update(float dt){
+		
+		time += dt;
+		
+		if(time > velocity){
+			time  = 0;
+			index ++;
+			if(index >= frames.length){
+				running = false;
+				index = 0;
+			}
+		}
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public Vector2D getPosition() {
+		return position;
+	}
+	
+	public BufferedImage getCurrentFrame(){
+		return frames[index];
+	}
+	
+	
+	
+	
+	
+}
